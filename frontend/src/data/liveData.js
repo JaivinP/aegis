e342e4a855bc5d5e6970f3a13a197b4b17e7c0ce
Liam Hanrahan <outercloudstudio@gmail.com>
2026-04-26 06:48:43 -0400

    better live data

--- frontend/src/data/liveData.js
+++ frontend/src/data/liveData.js
@@ -5,20 +5,20 @@ const dataUrl = 'http://34.41.16.247:8080'
 export function useLiveData() {
   const [data, setData] = useState({
         "acceleration": {
-            "x": 20.564446496274158,
-            "y": 89.3409753196091,
-            "z": 27.01100953100555
+            "x": 0,
+            "y": 0,
+            "z": 0
         },
         "gyro": {
-            "x": 78.43000475987452,
-            "y": 12.889991753377162,
-            "z": 49.67267611307297
+            "x": 0,
+            "y": 0,
+            "z": 0
         },
-        "humidity": 10.22729554389683,
-        "light": 41.39773707326412,
-        "shockDetected": 69.58334054644678,
-        "temperature": 46.778468378764835,
-        "water": 50.96164793214775
+        "humidity": 10,
+        "light": 10,
+        "shockDetected": 0,
+        "temperature": 20,
+        "water": 0
     });
 
   useEffect(() => {
